a94532d1c784c9fbbaaba2f47b39cd93eaca3a43
sebleyx <seb.leyx@gmail.com>
2016-03-01 17:50:51 +0100

    mise a jour

--- jeu/main.c
+++ jeu/main.c
@@ -18,7 +18,7 @@ int aleatoire(int max){
 }
 
 
-void casPosible(bool small,bool horizontal,int *x,int *y){
+void casPossible(bool small,bool horizontal,int *x,int *y){
   if(horizontal){
     if(small){
       *x=aleatoire(5);
@@ -48,28 +48,17 @@ bool booleatoire(void){
 
 
 piece* creerPieces(int nombrePiece){
-  printf("nombrePiece : %d \n",nombrePiece);
-  printf("fonction creerPiece \n");
   piece* tab =(piece*)malloc(nombrePiece * sizeof(piece));
-  printf("tab allouer \n");
   srand(time(NULL));
-  void (*pointeurSurFonction)(bool,bool,int*,int*);
-  pointeurSurFonction = casPosible;
-  printf("on va rentrer dans boucle1 \n");
   for(int i=0;i<nombrePiece;i++){
-    printf("i= %d\n",i);
     bool small = booleatoire();
     bool horizontal = booleatoire();
     int x;
     int y;      
-    printf("on rentre dans le pointeur \n");
-    (*pointeurSurFonction)(small,horizontal,&x,&y);
-    printf("on sort \n");
+    casPossible(small,horizontal,&x,&y);
     tab[i]=new_piece_rh(x,y,small,horizontal);
-    printf("on va verifier intersect \n");
     int j=0;
     for(j=0;j<i;j++){
-      printf("j= %d \n",j);
       if(i!=j){
 	if(intersect((cpiece)tab[i],(cpiece)tab[j])==false){
 	  continue;
@@ -79,12 +68,10 @@ piece* creerPieces(int nombrePiece){
 	}
       }
     }
-    printf("j= %d \n",j);
     if(j!=i){
       i--;
       continue;
     }
-    printf("on augmente i \n");
   }
   return tab;
 }
@@ -93,7 +80,7 @@ void affichage(game g){
   int nb = game_nb_pieces(g);
   for(int i=0;i<nb;i++){
     cpiece p = game_piece(g,i);
-    printf(" piece %d x:%d y:%d small:%d horizontal:%d \n",i,(*p).x,(*p).y,(*p).small,(*p).horizontal);
+    printf(" piece %d x:%d y:%d small:%d horizontal:%d \n",i,get_x(p),get_y(p),(get_width(p)*get_height(p))==2,is_horizontal(p));
   }
 }
 
@@ -101,8 +88,11 @@ int main(int argc,char *argv[]){
   if(argc!=2) usage(argv[0]);
   int nbPiece = atoi(argv[1]);
   if(nbPiece<=0 || nbPiece>PieceMax) usage(argv[0]);
+  printf("on creerPiece \n");
   piece* grille = creerPieces(nbPiece);
+  printf("creerPiece fonctionne \n");
   game rushHour = new_game_hr(nbPiece,grille);
-  affichage(rushHour);
-  printf("ok");
+  printf("on a creer newGame \n");
+  //affichage(rushHour);
+  printf("ok \n");
 }  
