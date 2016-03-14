56aa6c60626b214789c21c48715867a0385ca40c
sebleyx <seb.leyx@gmail.com>
2016-03-14 11:44:57 +0100

    anneRouge

--- aneRouge/aneRouge.c
+++ aneRouge/aneRouge.c
@@ -4,7 +4,9 @@
 #include "game.h"
 #include "piece.h"
 #include "string.h"
-#define valeurCaseVide -1
+#define WIDTH 4
+#define HEIGHT 5
+#define VALEUR_CASE_VIDE -1
 
 piece* allouer_Tab_Piece(int nbPiece){
   piece* allouer = (piece*)malloc(nbPiece*sizeof(piece));
@@ -16,11 +18,9 @@ void affichage(cgame g){
   printf("Nombre de mouvements effectués : %d \n",game_nb_moves(g));
   printf("\n");
   int nb = game_nb_pieces(g);
-  int WIDTH = game_width(g);
-  int HEIGHT = game_height(g);
   int tab_game[WIDTH][HEIGHT];
   for(int i=0; i<WIDTH; i++){
-    for(int j=0; j<HEIGHT; j++) tab_game[i][j]= valeurCaseVide;
+    for(int j=0; j<HEIGHT; j++) tab_game[i][j]= VALEUR_CASE_VIDE;
   }
   for(int i=0;i<nb;i++){
     cpiece tmp_piece = game_piece(g,i);
@@ -34,7 +34,7 @@ void affichage(cgame g){
   for(int yt=HEIGHT-1; yt>=0; yt--){
     printf("%d ",yt);
     for(int xt=0; xt<WIDTH; xt++){
-      if(tab_game[xt][yt]==valeurCaseVide){
+      if(tab_game[xt][yt]==VALEUR_CASE_VIDE){
 	printf("[ ");
       }
       else{
@@ -52,10 +52,8 @@ void affichage(cgame g){
   printf("\n");
 }
 
-piece* creer_Pieces_niveau_1(int *width_game,int *height_game,int* nbPieces){
+piece* creer_Pieces_niveau_1(int* nbPieces){
   *nbPiece=10;
-  *width_game=4;
-  *height_game=5;
   piece* tableau1 = allouer_Tab_Piece(nbPiece);
   tableau1[0] = newpiece(1,1,2,2,true,true);
   tableau1[1] = newpiece(0,3,2,1,true,true);
@@ -70,10 +68,7 @@ piece* creer_Pieces_niveau_1(int *width_game,int *height_game,int* nbPieces){
   return tableau1;
 }
 
-piece* creer_Pieces_niveau_2(int *width_game,int *height_game,int* nbPieces){
-  *nbPiece=10;
-  *width_game=4;
-  *height_game=5;
+piece* creer_Pieces_niveau_2(int* nbPieces){
   piece* tableau2 = allouer_Tab_Piece(nbPiece);
   tableau1[0] = newpiece(1,3,2,2,true,true);
   tableau1[1] = newpiece(0,3,1,2,true,true);
@@ -88,10 +83,8 @@ piece* creer_Pieces_niveau_2(int *width_game,int *height_game,int* nbPieces){
   return tableau2;
 }
 
-piece* creer_Pieces_niveau_3(int *width_game,int *height_game,int* nbPieces){
+piece* creer_Pieces_niveau_3(int* nbPieces){
   *nbPiece=10;
-  *width_game=4;
-  *height_game=5;
   piece* tableau3 = allouer_Tab_Piece(nbPiece);
   tableau1[0] = newpiece(1,3,2,2,true,true);
   tableau1[1] = newpiece(0,4,1,1,true,true);
@@ -107,13 +100,13 @@ piece* creer_Pieces_niveau_3(int *width_game,int *height_game,int* nbPieces){
 }
 
 
-piece* niveau_pieces(int niveau,int *width_game,int *height_game,int* nbPieces){
+piece* niveau_pieces(int niveau,int* nbPieces){
   switch(niveau){
   case 1:
-    piece* tab=creer_Pieces_niveau_1(&width_game,&height_game,&nbPieces);
+    piece* tab=creer_Pieces_niveau_1(&nbPieces);
     break;
   case 2:
-    piece* tab=creer_Pieces_niveau_2(&width_game,&height_game,&nbPieces);
+    piece* tab=creer_Pieces_niveau_2(&nbPieces);
     break;
   case 3:
     piece* tab=creer_Pieces_niveau_3(&width_game,&height_game,&nbPieces);
@@ -126,16 +119,32 @@ piece* niveau_pieces(int niveau,int *width_game,int *height_game,int* nbPieces){
 
 int main(int argc,char* argv[]){
   char rejouer[6];
-  int niveau,width_game,height_game,nbPieces;
+  int niveau,nbPieces;
   printf("choisissez parmis les 3 niveaux \n niveau 1 tapez 1 \n niveau 2 tapez 2 \n niveau 3 tapez 3\n");
   scanf("%d",&niveau);
   while(niveau!=1 && niveau !=2 && niveau!=3){
     printf("choisissez parmis les niveaux proposés !\n");
     scanf("%d",&niveau);
   }
-  piece* grille = niveau_pieces(niveau,&width_game,&height_game,&nbPieces);
-  game aneRouge = new_game(width_game,height_game,nbPieces,grille);
+  piece* grille = niveau_pieces(niveau,&nbPieces);
+  game aneRouge = new_game(WIDTH,HEIGHT,nbPieces,grille);
   affichage((cgame)aneRouge);
   startGame(aneRouge,nbPieces);
+  printf("Voulez vous rejouez? oui/non \n");
+  fgets(rejouer,6,stdin);
+  while(strcmp(rejouer,"oui\n")==0){
+    printf("choisissez parmis les 3 niveaux \n niveau 1 tapez 1 \n niveau 2 tapez 2 \n niveau 3 tapez 3\n");
+    scanf("%d",&niveau);
+    while(niveau!=1 && niveau !=2 && niveau!=3){
+      printf("choisissez parmis les niveaux proposés !\n");
+      scanf("%d",&niveau);
+    }
+    piece* grille = niveau_pieces(niveau,&nbPieces);
+    game anneRouge = new_game(WIDTH,HEIGHT,nbPieces,grille);
+    affichage((cgame)anneRouge);
+    startGame(rushHour,nbPiece);
+    printf("Voulez vous rejouez? oui/non \n");
+    fgets(rejouer,6,stdin);
+  }
   return EXIT_SUCCESS;
 }
