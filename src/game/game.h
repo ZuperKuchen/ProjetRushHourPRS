97cd7b0b528e92188fb5da01f1965bdd4f024f4e
PierrePicom <chauveau.pierre@live.com>
2016-03-14 12:13:11 +0100

    maj

--- src/game/game.h
+++ src/game/game.h
@@ -36,9 +36,19 @@ typedef const struct game_s* cgame;
  * @param nb_pieces number of pieces g
  * @param pieces array of pieces. The pieces given here are not modified by this function.
  * @return a pointer toward the generated game
+ *
+
+/**
+ * @brief Create a new game given a starting position defined by a set of pieces.
+ * The pieces of the created game are copies of the ones given as argument.
+ * The number of moves of the game is set to 0.
+ * @param nb_pieces number of pieces g
+ * @param pieces array of pieces. The pieces given here are not modified by this function.
+ * @return a pointer toward the generated game
  */
-// game new_game_hr (int nb_pieces, piece *pieces);
-// désormais dans game_rh.c & game_rh.h
+
+game new_game_hr (int nb_pieces, piece *pieces);
+
 
 /**
  * @brief Destroy the game and free allocated memory
@@ -70,8 +80,14 @@ cpiece game_piece(cgame g, int piece_num);
  * @brief Test is the game is over (i.e. if the piece 0 has reached the exit).
  * @return true if the piece number 0 has coordinates (4,3)
 */
-// bool game_over_hr(cgame g);
-// desormais dans game_rh
+
+
+/**
+ * @brief Test is the game is over (i.e. if the piece 0 has reached the exit).
+ * @return true if the piece number 0 has coordinates (4,3)
+*/
+
+bool game_over_hr(cgame g);
 
 
 /**
