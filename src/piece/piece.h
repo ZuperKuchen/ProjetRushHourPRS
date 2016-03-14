bd605ec3bff2334e11caece8df3d6db10b35cc09
PierrePicom <chauveau.pierre@live.com>
2016-03-14 11:50:47 +0100

    maj

--- src/piece/piece.h
+++ src/piece/piece.h
@@ -33,16 +33,6 @@ typedef const struct piece_s* cpiece;
 typedef enum dir_e {UP, LEFT, DOWN, RIGHT} dir;
 
 /**
- * @brief Creates a new piece.
- * @param x,y coordinates of the lower-left corner (Remark: x-axe is oriented from left to right and y-axe is oriented from bottom to top).
- * @param small indicates if the vehicle is of size 2 (small=true) or 3 (small=false)
- * @param horizontal indicates whether the vehicle can move horizontally or vertically
- * @return a pointer toward the generated piece
- */
-piece new_piece_rh (int x, int y, bool small, bool horizontal);
-
-
-/**
  * @brief Destroys the piece and frees allocated memory
  * @param p the piece to destroy
  */
