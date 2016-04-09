a652daed5a9b17aec155b3cfc344e2d0429eef7d
sebleyx <seb.leyx@gmail.com>
2016-04-09 22:33:15 +0200

    mise a jour

--- solveur/solveur.c
+++ solveur/solveur.c
@@ -4,112 +4,8 @@
 #include <stdbool.h>
 #include <game.h>
 #include <piece.h>
-#define GAME_SIZE_RUSHHOUR 6
-#define EMPTY_CASE_VALUE -1
-#define WIDTH 4
-#define HEIGHT 5
-
-void display_initRH(cgame g,int nb,int tab_game[GAME_SIZE_RUSHHOUR][GAME_SIZE_RUSHHOUR]){
-  for(int i=0; i<GAME_SIZE_RUSHHOUR; i++){
-    for(int j=0; j<GAME_SIZE_RUSHHOUR; j++){
-      tab_game[i][j]= EMPTY_CASE_VALUE;
-    }
-  }
-  for(int i=0;i<nb;i++){
-    cpiece tmp_piece = game_piece(g,i);
-    int x = get_x(tmp_piece);
-    int y = get_y(tmp_piece);
-    if(is_horizontal(tmp_piece)){
-      for(int j=0; j<get_width(tmp_piece); j++){
-	tab_game[x+j][y] = i;
-      }
-    }else{
-      for(int j=0; j<get_height(tmp_piece); j++){
-	tab_game[x][y+j] = i;
-      }
-    }
-  }
-}
-
-void display_casesRH(cgame g,int nb,int tab_game[GAME_SIZE_RUSHHOUR][GAME_SIZE_RUSHHOUR]){
-  for(int yt=GAME_SIZE_RUSHHOUR-1; yt>=0; yt--){
-    printf("%d ",yt);
-    for(int xt=0; xt<GAME_SIZE_RUSHHOUR; xt++){
-      if(tab_game[xt][yt]==EMPTY_CASE_VALUE){
-	printf("[ ");
-      }
-      else{
-	printf("[%d", tab_game[xt][yt]);
-      }
-      if(xt==5 && yt==3){
-	printf("{");
-      }else{
-	printf("]");
-      }
-    }
-    printf("\n");
-  }
-  printf("  ");
-  for(int k=0;k<GAME_SIZE_RUSHHOUR;k++) printf(" %d ",k);
-  printf("\n\n");
-}
-
-
-void displayRH(cgame g){
-  printf("\n");
-  int nb = game_nb_pieces(g);
-  int tab_game[GAME_SIZE_RUSHHOUR][GAME_SIZE_RUSHHOUR];
-  display_initRH(g,nb,tab_game);
-  display_casesRH(g,nb,tab_game);
-  printf("\n");
-}
-void cases_displayAR(cgame g,int nb,int tab_game[WIDTH][HEIGHT]){     
-  for(int yt=HEIGHT-1; yt>=0; yt--){
-    printf("%d ",yt);
-    for(int xt=0; xt<WIDTH; xt++){
-      if(tab_game[xt][yt]==EMPTY_CASE_VALUE){
-	printf("[ ");
-      }
-      else{
-	printf("[%d", tab_game[xt][yt]);
-      }
-      printf("]");
-    }
-    printf("\n");
-  }
-  printf("  ");
-  printf("***      ***\n");
-  printf(" ");
-  for(int k=0;k<WIDTH;k++) printf("  %d",k);
-  printf("\n\n");
-}
-
-void init_displayAR(cgame g,int nb,int tab_game[WIDTH][HEIGHT]){     
-  for(int i=0; i<WIDTH; i++){
-    for(int j=0; j<HEIGHT; j++) tab_game[i][j]=EMPTY_CASE_VALUE ;
-  }
-  for(int i=0;i<nb;i++){
-    cpiece tmp_piece = game_piece(g,i);
-    int x = get_x(tmp_piece);
-    int y = get_y(tmp_piece);
-    for(int j=0; j<get_height(tmp_piece); j++){
-      for(int k=0;k<get_width(tmp_piece);k++) tab_game[x+k][y+j] = i;
-    }
-  }
-}
-
-void displayAR(cgame g){
-  printf("\n");
-  printf("Nombre de mouvements effectués : %d \n",game_nb_moves(g));
-  printf("\n");
-  int nb = game_nb_pieces(g);
-  int tab_game[WIDTH][HEIGHT];
-  init_displayAR(g,nb,tab_game);
-  printf("  ************\n");
-  cases_displayAR(g,nb,tab_game);
-  printf("Pour quitter appuyez sur q \n");
-  printf("\n");
-}
+#include "displayRH.h"
+#include "displayAR.h"
 
 void grid_size(FILE *file,int *width,int *height){
   int line[5];
@@ -147,6 +43,7 @@ piece* read_Config_txtRH(int *width,int *height,int *nbPieces){
   grid_nbPieces(file,nbPieces);
   piece* tableau = (piece*)malloc((*nbPieces) * sizeof(piece));
   create_grid(file,*nbPieces,tableau);
+  fclose(file);
   return tableau;
 }
 
@@ -160,6 +57,7 @@ piece* read_Config_txtAR(int *width,int *height,int *nbPieces){
   grid_nbPieces(file,nbPieces);
   piece* tableau = (piece*)malloc((*nbPieces) * sizeof(piece));
   create_grid(file,*nbPieces,tableau);
+  fclose(file);
   return tableau;
 }
  
@@ -167,18 +65,28 @@ int main(int argc,char* argv[]){
   int width = 0;
   int height = 0;
   int nbPieces = 0;
-  int jeu = atoi(argv[1]);
-  if(jeu==0){
-    piece *grid = read_Config_txtRH(&width,&height,&nbPieces);
-    game rushHour = new_game_hr(nbPieces,grid);
-    displayRH((cgame)rushHour);
+  char choose_game[3];
+  bool good = false;
+  while(!good){
+    printf("\n Choisissez :\n   -0 pour rushhour\n   -1 pour anerouge\n");
+    fgets(choose_game,3,stdin);
+    int number_of_game = atoi(choose_game);  
+    if(number_of_game == 0){
+      piece *grid = read_Config_txtRH(&width,&height,&nbPieces);
+      game rushHour = new_game_hr(nbPieces,grid);
+      displayRH((cgame)rushHour);
+      delete_game(rushHour);
+      good = true;
+    }
+    else if(number_of_game==1){
+      piece *grid = read_Config_txtAR(&width,&height,&nbPieces);
+      game aneRouge = new_game(width,height,nbPieces,grid);
+      displayAR((cgame)aneRouge);
+      delete_game(aneRouge);
+      good = true;
+    }
+    else return EXIT_FAILURE;
   }
-  else if(jeu==1){
-    piece *grid = read_Config_txtAR(&width,&height,&nbPieces);
-    game aneRouge = new_game(width,height,nbPieces,grid);
-    displayAR((cgame)aneRouge);
-  }
-  else return EXIT_FAILURE;
   return EXIT_SUCCESS;
 }
   
