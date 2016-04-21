099d7a1cd31e60d071d1bebd08cf53c5c370629f
Rémi Brisset <remibrisset@gmail.com>
2016-04-21 19:39:19 +0200

    Maj affichage

--- rushHour/rushHourGraphic.c
+++ rushHour/rushHourGraphic.c
@@ -9,8 +9,43 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include "config_sdl.h"
-#define WINDOW_SIZE 600
+#define WINDOW_SIZE 800
 
+void pieces_graphic_position(SDL_Rect **pos_pieces, piece pieces[],int nb_pieces){
+  for (int i=0; i<nb_pieces; i++){
+    pos_pieces[i]->x=get_x((cpiece) pieces[i]) * 100 + 100;
+    pos_pieces[i]->y=(- get_y((cpiece) piece[i]) + 6) * 100 + 100;
+    pos_pieces[i]->w=get_width((cpiece) piece[i])*100;
+    pos_pieces[i]->h=get_height((cpiece) piece[i])*100;
+  }
+}
+
+bool cars_display(SDL_Renderer renderer,game g){
+  int nb_pieces=game_nb_pieces(g);
+  SDL_Rect **pos_pieces;
+  piece_graphic_position(SDL_Rect pos_pieces, , 
+  
+
+
+
+
+/*
+void start_game_graphic(game g,int nbPiece, ){
+  while(!game_over_hr(g)){
+    dir direction;
+    int num = choose_number_piece(nbPiece,g);
+    int test = choose_direction(g,num,&direction);
+    if (!test) continue;
+    int distance = choose_distance(g);
+    bool goodMove=play_move(g,num,direction,distance);
+    if (goodMove==false) {
+      printf("\n! Déplacement impossible !\n\n");
+      continue;
+    }
+    display(g);
+  }
+}
+*/
 
 int main(int argc,char **argv){
   SDL_Window *screen = NULL;
@@ -34,37 +69,30 @@ int main(int argc,char **argv){
     SDL_Quit();
     return EXIT_FAILURE;
   }
-
-
-  
-  /* SDL_Renderer *menu = SDL_CreateRenderer(screen,-1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_TARGETTEXTURE);
-  if (menu == NULL){
-    printf("NTMMTN");
+  SDL_Renderer *menu_renderer = SDL_CreateRenderer(screen,-1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_TARGETTEXTURE);
+  if (menu_renderer == NULL){
+    fprintf(stderr, "Erreur d'initialisation du renderer : %s\n", SDL_GetError());
   }
-  */
-  
+  SDL_Surface *menu_sprite = IMG_Load("../../rushHour/titleScreen.jpg");
+  SDL_Texture *menu_texture = SDL_CreateTextureFromSurface(menu_renderer,menu_sprite);
+  SDL_FreeSurface(menu_sprite);
   SDL_Rect menu_pos= {0, 0, WINDOW_SIZE, WINDOW_SIZE};
-  /*SDL_Texture *texture_menu = IMG_LoadTexture(menu,"RushHourMenu.png");
-  if (texture_menu == NULL){
-    printf("NTM");
-  }
-
-  SDL_RenderClear(menu);
-  SDL_RenderCopy(menu, texture_menu, NULL, &menu_pos);
-  SDL_RenderPresent(menu);
-  */
-  SDL_Surface *screen_surf = SDL_GetWindowSurface(screen);
-  SDL_Surface *menu_surf = IMG_Load("../../rushHour/RushHourMenu.png");
-  SDL_BlitSurface(menu_surf, NULL, screen_surf, &menu_pos);
-  SDL_UpdateWindowSurface(screen);
-
+  SDL_RenderCopy(menu_renderer, menu_texture, NULL, &menu_pos);
+  SDL_RenderPresent(menu_renderer);
+  //if (texture_menu == NULL){
   
+
+
+
+
+
+  //FinTest
   while(continuer){
     SDL_WaitEvent(&event);
     if(event.type == SDL_QUIT){
       continuer=false;
+      SDL_DestroyRenderer(menu_renderer);
       SDL_DestroyWindow(screen);
-      //SDL_DestroyRenderer(menu);
       SDL_Quit();
     }
   }
